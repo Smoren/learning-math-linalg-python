c562a1f420ac0f6abbdfd99ccd140bdbdd256aee
Smoren <ofigate@gmail.com>
2025-12-28 21:46:27 +0300

    upd

--- linalg/analyzers.py
+++ linalg/analyzers.py
@@ -4,11 +4,28 @@ from linalg.system import LinearSystem
 from linalg.utils import is_zero
 
 
+class MatrixBaseAnalyser:
+    _matrix: np.ndarray
+
+    def __init__(self, matrix: np.ndarray):
+        self._matrix = matrix
+
+    def is_square(self) -> bool:
+        """Проверяет, является ли матрица квадратной (NxN)."""
+        return self._matrix.shape[0] == self._matrix.shape[1]
+
+    def is_singular(self) -> bool:
+        """Проверяет, является ли матрица вырожденной (определитель равен 0)."""
+        return not self.is_square() or is_zero(np.linalg.det(self._matrix))
+
+
 class LinearSystemBaseAnalyser:
     _linear_system: LinearSystem
+    _matrix_analyser: MatrixBaseAnalyser
 
     def __init__(self, linear_system: LinearSystem):
         self._linear_system = linear_system
+        self._matrix_analyser = MatrixBaseAnalyser(linear_system.A)
 
     def is_homogeneous(self) -> bool:
         """Проверяет, является ли система однородной (B = 0)."""
@@ -16,8 +33,8 @@ class LinearSystemBaseAnalyser:
 
     def is_square(self) -> bool:
         """Проверяет, является ли система квадратной (NxN)."""
-        return self._linear_system.A.shape[0] == self._linear_system.A.shape[1]
+        return self._matrix_analyser.is_square()
 
     def is_singular(self) -> bool:
         """Проверяет, является ли система вырожденной (определитель A равен 0)."""
-        return not self.is_square() or is_zero(np.linalg.det(self._linear_system.A))
+        return not self._matrix_analyser.is_singular()
