052cab0846d6e789d1c2cfca4c59e26d5d863a3a
Smoren <ofigate@gmail.com>
2025-12-31 04:54:40 +0300

    First test works!

--- tests/transformers/gauss/gauss_test.py
+++ tests/transformers/gauss/gauss_test.py
@@ -15,5 +15,5 @@ def test_gauss_for_invertible(data: tuple[np.ndarray, np.ndarray, np.ndarray]):
     transformer = LinearSystemGaussTransformer(linear_system)
     transformer.apply_gauss()
 
-    assert np.isclose(linear_system.A, np.eye(A.shape[0], A.shape[1])).all()
-    assert np.isclose(linear_system.B, B_expected).all()
+    np.testing.assert_array_equal(linear_system.A, np.eye(A.shape[0], A.shape[1]))
+    np.testing.assert_array_equal(linear_system.B, B_expected)
