0a5526aab7f8342de554bc2e9b5033ac4d3c1e6a
Smoren <ofigate@gmail.com>
2025-12-31 13:54:19 +0300

    upd

--- tests/transformers/gauss/fixtures.py
+++ tests/transformers/gauss/fixtures.py
@@ -3,7 +3,7 @@ from typing import Generator
 import numpy as np
 
 
-def data_provider_for_gauss() -> Generator[tuple[np.ndarray, np.ndarray, np.ndarray], None, None]:
+def data_provider_for_gauss_for_invertible() -> Generator[tuple[np.ndarray, np.ndarray, np.ndarray], None, None]:
     yield from [
         (
             np.array([
@@ -43,6 +43,37 @@ def data_provider_for_gauss() -> Generator[tuple[np.ndarray, np.ndarray, np.ndar
                 [0.5],
                 [4.0],
                 [0.375],
+            ], dtype=np.float64),
+        ),
+    ]
+
+
+def data_provider_for_gauss_for_singular() -> Generator[tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray], None, None]:
+    yield from [
+        (
+            np.array([
+                [0, 2, 0, 0],
+                [4, 0, 1, 0],
+                [0, 0, 0, 8],
+                [4, 0, 1, 0],
+            ], dtype=np.float64),
+            np.array([
+                [1],
+                [2],
+                [3],
+                [4],
+            ], dtype=np.float64),
+            np.array([
+                [1, 0, 0.25, 0],
+                [0, 1, 0, 0],
+                [0, 0, 0, 1],
+                [0, 0, 0, 0],
+            ], dtype=np.float64),
+            np.array([
+                [0],
+                [0],
+                [0],
+                [1],
             ], dtype=np.float64),
         ),
     ]
--- tests/transformers/gauss/gauss_test.py
+++ tests/transformers/gauss/gauss_test.py
@@ -1,17 +1,19 @@
 import numpy as np
 import pytest
 
+from app.analysers import MatrixAnalyser, EchelonMatrixAnalyser
 from app.system import LinearSystem
 from app.transformers import LinearSystemGaussTransformer
-from tests.transformers.gauss.fixtures import data_provider_for_gauss
+from tests.transformers.gauss.fixtures import data_provider_for_gauss_for_invertible, \
+    data_provider_for_gauss_for_singular
 
 
-@pytest.mark.parametrize("data", data_provider_for_gauss())
+@pytest.mark.parametrize("data", data_provider_for_gauss_for_invertible())
 def test_gauss_for_invertible(data: tuple[np.ndarray, np.ndarray, np.ndarray]):
     """
     Тестирование метода Гаусса для решения СЛАУ с обратимой матрицей.
 
-    Проверяет, что метод Гаусса корректно преобразует систему в ступенчатый вид
+    Проверяет, что метод Гаусса корректно преобразует систему в улучшенный ступенчатый вид
     и находит правильное решение.
 
     Args:
@@ -19,13 +21,6 @@ def test_gauss_for_invertible(data: tuple[np.ndarray, np.ndarray, np.ndarray]):
               - A: Матрица коэффициентов СЛАУ
               - B: Вектор свободных членов
               - X: Ожидаемый вектор решения
-
-    Внутри теста:
-        1. Создается объект LinearSystem с заданными матрицами A и B
-        2. Создается трансформер LinearSystemGaussTransformer
-        3. Применяется метод Гаусса
-        4. Проверяется, что матрица A превратилась в единичную
-        5. Проверяется, что вектор B совпадает с ожидаемым решением
     """
     A, B, X = data
 
@@ -39,3 +34,37 @@ def test_gauss_for_invertible(data: tuple[np.ndarray, np.ndarray, np.ndarray]):
 
     # Проверка, что решение совпадает с ожидаемым
     np.testing.assert_array_equal(linear_system.B, X)
+
+
+@pytest.mark.parametrize("data", data_provider_for_gauss_for_singular())
+def test_gauss_for_singular(data: tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]):
+    """
+    Тестирование метода Гаусса для решения СЛАУ с вырожденной матрицей.
+
+    Проверяет, что метод Гаусса корректно преобразует систему в улучшенный ступенчатый вид.
+
+    Args:
+        data: Кортеж из трех numpy массивов:
+              - A: Матрица коэффициентов СЛАУ
+              - B: Вектор свободных членов
+              - A': Улучшенный ступенчатый вид матрицы A
+              - B': Матрица B после преобразований
+    """
+    A, B, A_, B_ = data
+
+    linear_system = LinearSystem(A, B)
+
+    transformer = LinearSystemGaussTransformer(linear_system)
+    transformer.apply_gauss()
+
+    # Проверка, что матрица приведена к ступенчатому виду
+    assert MatrixAnalyser(linear_system.A).is_echelon()
+
+    # Проверка, что матрица приведена к улучшенному ступенчатому виду
+    assert EchelonMatrixAnalyser(linear_system.A).is_reduced_echelon()
+
+    # Проверка, что левая матрица соответствует ожидаемому
+    np.testing.assert_array_equal(linear_system.A, A_)
+
+    # Проверка, что решение совпадает с ожидаемым
+    np.testing.assert_array_equal(linear_system.B, B_)
